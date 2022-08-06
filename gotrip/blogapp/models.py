36163b16e3be55ae75c630074ab8f0321fc14896
p6trick <juniyook@korea.ac.kr>
2022-08-06 19:25:23 +0900

    checklist

--- gotrip/blogapp/models.py
+++ gotrip/blogapp/models.py
@@ -3,11 +3,11 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    체크리스트1 = models.CharField(max_length=100)
-    체크리스트2 = models.CharField(max_length=100)
-    체크리스트3 = models.CharField(max_length=100)
-
     제목 = models.CharField(max_length=200)
+    체크리스트1 = models.CharField(max_length=100, default='')
+    체크리스트2 = models.CharField(max_length=100, default='')
+    체크리스트3 = models.CharField(max_length=100, default='')
+
     이미지 = models.ImageField(blank=False, upload_to='blog_photo')
     내용 = models.TextField()
     createDate = models.DateTimeField(auto_now_add=True)
