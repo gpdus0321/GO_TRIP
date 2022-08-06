d82e6e65682c5168cbcdd4862f6520d48c2f07ed
p6trick <juniyook@korea.ac.kr>
2022-08-06 19:07:38 +0900

    before_models

--- gotrip/blogapp/forms.py
+++ gotrip/blogapp/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['제목', '이미지', '내용']
+        fields = ['제목', '체크리스트1','체크리스트2','체크리스트3','이미지', '내용']
--- gotrip/blogapp/models.py
+++ gotrip/blogapp/models.py
@@ -3,6 +3,10 @@ from django.urls import reverse
 
 
 class Post(models.Model):
+    체크리스트1 = models.CharField(max_length=100)
+    체크리스트2 = models.CharField(max_length=100)
+    체크리스트3 = models.CharField(max_length=100)
+
     제목 = models.CharField(max_length=200)
     이미지 = models.ImageField(blank=False, upload_to='blog_photo')
     내용 = models.TextField()
