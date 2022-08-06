34d2c7acec53612440b5c5219d3f87404099c1e8
seoyc0819 <105352087+seoyc0819@users.noreply.github.com>
2022-08-06 10:32:46 +0900

    Update urls.py

--- gotrip/gotrip/urls.py
+++ gotrip/gotrip/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('serviceapp.' 'urls')),
+    path('blog/', include('blogapp.' 'urls')),
 ]
